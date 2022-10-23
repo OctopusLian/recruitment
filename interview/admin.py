2566624b55399e491e1dd4468d145f42a0f0ead4
OctopusLian <zoctopus@qq.com>
2022-10-23 18:24:16 +0800

    候选人列表筛选和查询

--- interview/admin.py
+++ interview/admin.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib import admin
 
 from interview.models import Candidate
@@ -13,5 +15,21 @@ class CandidateAdmin(admin.ModelAdmin):
         'second_score','second_result', 'second_interviewer_user','hr_score', 'hr_result','hr_interviewer_user',
         'last_editor',
     )
+    # 右侧筛选条件
+    list_filter = ('city', 'first_result', 'second_result', 'hr_result', 'first_interviewer_user', 'second_interviewer_user',
+    'hr_interviewer_user')
+
+    # 查询字段
+    search_fields = ('username', 'phone', 'email', 'bachelor_school')
+
+    ### 列表页排序字段
+    ordering = ('hr_result', 'second_result', 'first_result')
+
+    def save_model(self, request, obj, form, change):
+        obj.last_editor = request.user.username
+        if not obj.creator:
+            obj.creator = request.user.username
+        obj.modified_date = datetime.now()
+        obj.save()
 
 admin.site.register(Candidate,CandidateAdmin)
