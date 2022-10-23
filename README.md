b80bb8551977663b2a91ad0eadfa865d0dc98c34
OctopusLian <zoctopus@qq.com>
2022-10-23 16:29:17 +0800

    add jobs model Job

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # 基于Django框架的招聘网站  
 
+## 运行  
+
+### 超级管理员  
+
+http://127.0.0.1:8000/admin/  
+账号：admin  
+邮箱：a@qq.com  
+密码：123456  
+
+
+## 常用操作  
+
+```
+# 运行
+python manage.py runserver
+
+# 同步表
+python manage.py makemigrations
+
+# 使改动生效
+python manage.py migrate
+```
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -10,6 +11,21 @@ JobTypes = [
     (4, "市场营销类")
 ]
 
+Cities = [
+    (0,"北京"),
+    (1,"上海"),
+    (2,"深圳"),
+    (3,"杭州"),
+    (4,"广州")
+]
+
 class Job(models.Model):
     # 不允许为空
     job_type = models.SmallIntegerField(blank=False,choices=JobTypes,verbose_name="职位类型")
+    job_name = models.CharField(max_length=250, blank=False, verbose_name="职位名称")
+    job_city = models.SmallIntegerField(choices=Cities, blank=False, verbose_name="工作地点")
+    job_responsibility = models.TextField(max_length=1024, verbose_name="职位职责")
+    job_requirement = models.TextField(max_length=1024, blank=False, verbose_name="职位要求")
+    creator = models.ForeignKey(User, verbose_name="创建人", null=True, on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(verbose_name="创建日期", auto_now_add=True) # 默认当前时间
+    modified_date = models.DateTimeField(verbose_name="修改日期", auto_now=True) # 默认当前时间
