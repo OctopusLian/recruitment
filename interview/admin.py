4104284b76b17019d5ebe52def02caab237a1857
OctopusLian <zoctopus@qq.com>
2022-10-25 13:37:06 +0800

    perf: 修改站点标题;设置字段提示;面试官信息与登录用户关联;设置一二面试官字段只读属性;HR角色可以在列表页面修改应聘者一二面试官

--- interview/admin.py
+++ interview/admin.py
@@ -65,6 +65,34 @@ class CandidateAdmin(admin.ModelAdmin):
     ### 列表页排序字段
     ordering = ('hr_result', 'second_result', 'first_result')
 
+    def get_list_editable(self, request):
+        group_names = self.get_group_names(request.user)
+        if request.user.is_superuser or 'hr' in group_names:
+            return ('first_interviewer_user', 'second_interviewer_user',)  # 返回可以批量编辑的字段给HR
+        return ()
+
+    def get_changelist_instance(self, request):
+        """
+        override admin method and list_editable property value
+        with values returned by our custom method implementation.
+        """
+        self.list_editable = self.get_list_editable(request)
+        return super(CandidateAdmin, self).get_changelist_instance(request)
+
+    def get_group_names(self, user):
+        group_names = []
+        for g in user.groups.all():
+            group_names.append(g.name)
+        return group_names
+
+    def get_readonly_fields(self, request, obj):
+        group_names = self.get_group_names(request.user)
+
+        if 'interviewer' in group_names:
+            logger.info("interviewer is in user's group for %s" % request.user.username)
+            return ('first_interviewer_user', 'second_interviewer_user',) # 设置字段只可以读
+        return ()
+
     def save_model(self, request, obj, form, change):
         obj.last_editor = request.user.username
         if not obj.creator:
